d4b7a173dc0958083a2e85166cc4e9c88914aeb1
Felipe Alves <favbalves@gmail.com>
2018-09-26 17:37:05 -0400

    Fix export

--- src/FluidFields.jl
+++ src/FluidFields.jl
@@ -4,7 +4,7 @@ using InplaceRealFFT, LinearAlgebra, FFTW
 using FluidTensors
 import FluidTensors
 
-export ScalarField, VectorField, SymmetricField, TracelessSymmetricField, isrealspace
+export ScalarField, VectorField, SymTrTenField, isrealspace
 
 const Float3264 = InplaceRealFFT.Float3264
 
